897d40bf12eea548683dcd8ed10ee28bbf791e45
Awais-S <163529360+Awais-S@users.noreply.github.com>
2024-04-01 22:01:31 +0500

    Update module.py

--- module.py
+++ module.py
@@ -1,12 +1,26 @@
 import random
 
-def asd():
-    rr = random.randint
-    rc = random.choice
-    wo = ['RMX1831','RMX1919', 'X657C','X662C','M2007J3SG','SM-G770F','SM-T867V','SM-S906U','LM-K735','SM-S908B','SM-G781N','SM-A205GN','J8110','RMX2071','SM-A025AZ','V2031','SM-A105M','Infinix X655B','LM-V350N','M2103K19PG','vivo 1940','BKL-L04','MAR-LX1A','J8210','V2102','SM-N9300','M2103K19PG','H60-L21','CPH2577','RMX3085','V2059','SM-X806B','SM-T540','SM-A528N','SM-T636B','LM-X420N','SM-A320FL','RMX3197','CPH1805']
-    ads = random.choice(wo)
-    uab = "Mozilla/5.0 (Linux; Android " + ads + " Build/" + str(random.randint(1000, 9999)) + ".0." + str(random.randint(1, 999)) + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
-    uaxb = f"Mozilla/5.0 (Linux; Android {str(rr(4,13))}; " + ads + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome 4.0/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 300)) + " Mobile Safari/537.36"
-    ua = str(rc([uab,uaxb]))
-    return ua
-asd()
+ugen = []
+for xd in range(5000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['5','6','7','8','9','10','11','12'])
+    if b in ['5', '6', '7', '8', '9']:
+        z=random.choice(['0', '1'])
+        bv=b+'.'+z+'.'+z
+    else:
+        bv=b
+    B=['GT-', 'SM-']
+    c=random.choice(B)
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    application_version = str(random.randint(111,396))+'.0.0.'+str(random.randrange(10,49))+'.'+str(random.randint(111,396))
+    V=str(random.randrange(11,99))
+    uas=f'{aa} {bv}; {c}{d}{e}{f} Build/{d}{f}{V}{f}; wv) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uas)
