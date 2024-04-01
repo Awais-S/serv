cd974827f11ac2aadac42eba783a7e07a2b738ab
Awais-S <163529360+Awais-S@users.noreply.github.com>
2024-04-01 22:55:22 +0500

    Update module.py

--- module.py
+++ module.py
@@ -1,6 +1,6 @@
 import random
 ugen=[]
-for agent in range(1):
+for agent in range(100000):
 		rr = random.randint
 		rc = random.choice
 		wo = ['V2120','SM-G973C', 'SM-A217N','M2006J10C','V2065','RMX1821','SM-T970','NAM-LX9','SM-T547U','LG-H870S','BQ-4030G','SPH-L720','LYA-L0C','SM-N9760','CPH1937','SM-T387W','SM-T503','M2010J19SG','LGV36','SM-A9200','RMX1809','CPH2063','SM-J810M','LG-RS988','CPH2495','SM-A505FN','SM-X205','V2110','EVR-L29','RMX2156','CPH3987','PPA-LX1','LDN-AL20','SM-M426B','BQ-5519G','V2118','LM-F100','SM-J701M','SM-T976B']
