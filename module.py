08b7f2df85c5fce0988e4fe849b0d2b85c7c8e11
Awais-S <163529360+Awais-S@users.noreply.github.com>
2024-04-02 15:29:22 +0500

    Update module.py

--- module.py
+++ module.py
@@ -3,7 +3,7 @@ ugen=[]
 for agent in range(100000):
 		rr = random.randint
 		rc = random.choice
-		wo = ['RMX1901','SC-04C', 'RMX1931','V2026','RMX1941','G630-U10','X663','SM-G930F','GT-I9500','SM-T231','MLA-L11','SM-P613','SM-E225F','SM-A725F','SM-S901N','LM-X420','CPH2161','SM-A600P','SM-A105F','Redmi Note 9 Pro','V2029','V2020','V2028','SM-N981N','Lenovo L19041','DLT-A0','T21-EEA','ZTE 8010RU','CPH2473','SM-J737T','M2011K2G','CPH2353']
+		wo = ['RMX1919','SC-02C', 'CPH2145','CPH2201','X662B','MHA-L29','TRT-LX2','SM-A500H','2209116AG','OCE-AN50','SM-9005','SM-A115U1','SM-G611FN','SM-M115G','SM-G990B2','SM-A4260','SM-N975C','SM-A307GT','V2126','SM-G355M','SM-J415GN','SM-J530FM','V341U','TFY-LX2','V2105','Lenovo L38111','Lenovo YT-J706X','SM-A405FN','V2060','V2048','Lava Z66','X671B','V1829','SM-G892A','SM-F907FN','CPH2199','SM-T825Y','SM-F700U','M2012K11AC']
 		ads = random.choice(wo)
 		uab = "Mozilla/5.0 (Linux; Android " + ads + " Build/" + str(random.randint(1000, 9999)) + ".0." + str(random.randint(1, 999)) + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
 		uaxb = f"Mozilla/5.0 (Linux; Android {str(rr(4,13))}; " + ads + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome 4.0/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 300)) + " Mobile Safari/537.36"
