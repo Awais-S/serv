86c06e5c1dc5e06c9637e810d92caa1ce03bb25b
Awais-S <163529360+Awais-S@users.noreply.github.com>
2024-04-04 20:29:42 +0500

    Update module.py

--- module.py
+++ module.py
@@ -3,7 +3,7 @@ ugen = []
 for agent in range(100000):
 		rr = random.randint
 		rc = random.choice
-		wo = ['J8170','SM-T509','J8270','SM-A205S','XQ-BT52','LM-K735','V2102','SM-S906U','J8110','J8210','2201116TG','RMX2205','CTR-LX3','SM-A125FN','V30','RMX1851','LGM-V300L','XQ-AU51','SM-A805N','CPH2413','V1829','SM-F926B','RMO-NX1','SM-A6060','G720C','SM-M022M','S6-KC','X670C','JDN2-L09','T810S','CPH2527','HJC-LX9','X6810','V2070','SM-A305GT','LM-G820N','HRY-LX1T','V2231','BQ-5045L','SM-J701MT','22126RN91Y','SM-S906E','V2239','X627V','SM-G973N']
+		wo = ['SM-J105Y','CPH2293','RMX2193','SM-A505GN','SM-F926U1', 'RMX2151','LM-Q630','vivo 1910','SM-A705U','SM-A426U1','V2022','V2023','V2027','V2026','X6511','SM-G525F','LM-V450','CPH2137','CPH2505','CPH2251','ART-L28','SM-A805FN','X626','LM-X120','LG-H931','RMX2180','CPH2015','STF-AL10','J9210','M2104K10I','SM-T307U','V2207','SM-A035G','SM-W2022','LM-K200','CPH2457','JAD-LX9','M2101K9AI','BQ-5732L','MH-T6000','SM-J530YN','CPH2493','SM-G965T','CPH1907','Y1000','FLA-LX2','LM-V405EBW','SM-T387T','CPH2173','VOG-L09','VIA X20','TA-1012','LH9931','M2007J20CI','22041219NY','K30-T','SM-A320FL','JAT-L41','MGA-LX9']
 		ads = random.choice(wo)
 		uab = "Mozilla/5.0 (Linux; Android " + ads + " Build/" + str(random.randint(1000, 9999)) + ".0." + str(random.randint(1, 999)) + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
 		uaxb = f"Mozilla/5.0 (Linux; Android {str(rr(4,13))}; " + ads + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome 4.0/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 300)) + " Mobile Safari/537.36"
