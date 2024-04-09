c110643c8dfe8b2df81548c3acb15287058c0201
Awais-S <163529360+Awais-S@users.noreply.github.com>
2024-04-09 16:32:50 +0500

    Update module.py

--- module.py
+++ module.py
@@ -1,37 +1,41 @@
 import random
 ugen=[]
-for ua in range(10000):
-    rr = random.randint
-    rc = random.choice
-    a_m=rc(['11','13','6.0.1','6.0','7.0','14','12','9'])
-    m_m=rc(['MEIZU 17 Pro','V1930A','MEIZU 18 Pro','MEIZU 16Xs','MEIZU 17 Pro','ZTE A2121E','MZ-Infinix X663D','MZ-MEIZU 20 Pro','MX6','Meizu 6T','Infinix X6821','Infinix X687'])
-    b_m=rc(['MRA58K','P50-EEA','UKQ1.230917.001','TKQ1.221114.001','NRD90M','SP1A.210812.016','PKQ1.190616.001'])
-    c_m=f"{str(rr(42,123))}.0.{str(rr(3200,6500))}.{str(rr(11,199))}"
-    f_m=f"{str(rr(100,460))}.0.0.{str(rr(11,99))}.{str(rr(11,360))}"
-    a_o=rc(['13','14'])
-    m_o=rc(['CPH2219','CPH2207','CPH2099','RMX2189','CPH2499','CPH2337','RMX3261','vivo 1902','RMX2155','LM-G900'])
-    b_o=rc(['SP1A.210812.016','UKQ1.230924.001','TP1A.220905.001','UP1A.230620.001'])
-    c_o=f"{str(rr(120,123))}.0.{str(rr(6000,6500))}.{str(rr(11,150))}"
-    c_h=f"{str(rr(100,122))}.0.{str(rr(6000,6500))}.{str(rr(62,240))}"
-    a_w=rc(['5.1.1','12','7.1.1','9','8.1.0'])
-    m_w=rc(['V1990A','RMX2155','M2007J1SC','SM-T220','V2131','MOTO G STYLUS 5G','Lenovo TB-X606X','MOTO E20'])
-    b_w=rc(['SP1A.210812.016','P00610','O11019','LMY47V','QP1A.190711.020','NMF26F'])
-    c_w=f"{str(rr(62,123))}.0.{str(rr(2500,6500))}.{str(rr(20,150))}"
-    u1=f"Mozilla/5.0 (Linux; Android {str(rr(4, 13))}; {m_m} Build/{b_m}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{c_m} Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/{f_m};]"
-    u2=f"Mozilla/5.0 (Linux; Android {str(rr(4, 13))}; {m_o} Build/{b_o}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{c_o} Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/{f_m};]"
-    aV=str(random.choice(range(10,20)))
-    u4=f"Mozilla/5.0 (Windows; U; Windows NT {str(random.choice(range(5,10)))}.1; en-US) AppleWebKit/534.{aV} (KHTML, like Gecko) Chrome/"+str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
-    u3=f"Mozilla/5.0 (Linux; Android {str(rr(4, 13))}; {m_w} Build/{b_w}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{c_w} Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/{f_m};]"
-    aV=str(random.choice(range(10,20)))
-    A=f"Mozilla/5.0 (Windows; U; Windows NT {str(random.choice(range(5,10)))}.1; en-US) AppleWebKit/534.{aV} (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
-    bV=str(random.choice(range(1,36)))
-    bx=str(random.choice(range(34,38)))
-    bz=f"5{bx}.{bV}"
-    B=f"Mozilla/5.0 (Windows NT {str(random.choice(range(5,10)))}.{str(random.choice(['2','1']))}) AppleWebKit/{bz} (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
-    cV=str(random.choice(range(1,36)))
-    cx=str(random.choice(range(34,38)))
-    cz=f"5{cx}.{cV}"
-    C=f"Mozilla/5.0 (Windows NT 6.{str(random.choice(['2','1']))}; WOW64) AppleWebKit/{cz} (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
-    D=f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 140)) + " Mobile Safari/537.36"
-    ua=rc([u1,u2,u3,u4,A,B,C,D])
-    ugen.append(ua)
+for agents in range(10000):
+	rr = random.randint
+	rc = random.choice
+	
+	# Oppo device details
+	and_oppo = rc(['14', '11', '10'])
+	model_oppo = rc(['CPH1979', 'CPH1879', 'CPH2207','CPH2271','CPH2343', 'CPH2309', 'CPH2237', 'CPH1875', 'CPH2411', 'CPH2349', 'CPH1871', 'CPH1907', 'CPH2173', 'CPH1073', 'CPH2213', 'CP3504L'])
+	chrome_oppo = f"{str(rr(80, 444))}.0.{str(rr(3000, 6500))}.{str(rr(11, 499))}"
+	
+	# Infinix device details
+	models_inf = rc(['X652', 'X6826', 'X653', 'X626B', 'X682C','X650B'])
+	ch_inf = f"{str(rr(80, 999))}.0.{str(rr(3200, 6500))}.{str(rr(11, 999))}"
+	
+	# Facebook Hua device details
+	fb_hua = f"{str(rr(100, 490))}.0.0.{str(rr(11, 99))}.{str(rr(11, 990))}"
+	
+	# Vivo device details
+	and_vivo = f"{str(rr(11, 14))}"
+	models_vivo = rc(['V2109', 'V2205', 'V2020CA','V2111A', 'vivo 1908', 'vivo 1909','V1930','V2026', 'V2034A', 'V2036A', 'V2201', 'V2110', 'V2035', 'V1965A', 'V2109','V2048','V1914A'])
+	
+	# Realme device details
+	models_re = rc(['RMX1903', 'RMX1921', 'RMX1931','RMX3472','RMX2111', 'RMX3193', 'RMX2075', 'RMX2195', 'RMX3571','RMX3286'])
+	buld_re = rc(['QP1A.190711.020', 'QKQ1.190918.001', 'SP1A.210812.016', 'QTG3.200617.002', 'SQ3A.220705.0040', 'RP1A.200720.011', 'UKQ1.230924.001', 'UKQ1.230917.001', 'TP1A.220624.014'])
+	ch_re = f"{str(rr(120, 999))}.0.{str(rr(3000, 6500))}.{str(rr(11, 999))}"
+	
+	# Samsung device details
+	ad_sam = rc(['10', '7.0', '14', '11'])
+	models_sam = rc(['JKM-LX2', 'SM-J810GF','SM-M305M','SM-G611K','MNA-LX9','SM-C7000','RMX2025','SM-S136DL', 'LM-F100N', 'BQ-8077L', 'SM-M135FU','SM-S515DL', 'M2012K11AG','M2007J17C', 'V2214', 'LG-D855', 'SM-S115DL','SM-J610F','SM-S367VL','SM-A600A','AMN-LX2','SM-N915A'])
+	ch_sam = f"{str(rr(52, 124))}.0.{str(rr(2200, 6500))}.{str(rr(11, 199))}"
+	
+	# Generating user-agent strings
+	u1 = f"Mozilla/5.0 (Linux; Android {str(rr(4, 13))} {model_oppo} Build/{buld_re}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{chrome_oppo} Mobile Safari/537.36"
+	u2 = f"Mozilla/5.0 (Linux; Android {str(rr(4, 13))}; Infinix {models_inf} Build/{buld_re}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_inf} Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/{fb_hua};]"
+	u3 = f"Mozilla/5.0 (Linux; Android {str(rr(4, 13))}; {models_vivo}) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{chrome_oppo} Mobile Safari/537.36 VivoBrowser/18.9.0.0"
+	u4 = f"Mozilla/5.0 (Linux; Android {str(rr(4, 13))} {models_re} Build/{buld_re}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_re} Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/{fb_hua};]"
+	u5 = f"Mozilla/5.0 (Linux; Android {str(rr(4,13))}; {models_sam}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome 4.0/" + str(random.randint(50, 999)) + ".0." + str(random.randint(1000, 4900)) + "." + str(random.randint(40, 900)) + " Mobile Safari/537.36"
+	
+	ua = rc([u1, u2, u3, u4, u5])
+	ugen.append(ua)
